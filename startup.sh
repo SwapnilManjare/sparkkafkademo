067770d53f384971c2b3380b430a93313704a4e2
swapnilmanjare <swapnilmanjare4@gmail.com>
2024-04-19 19:43:27 +0530

    modified startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3 producer_script.py
-spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1 spark_kafka.py
+#spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1 spark_kafka.py

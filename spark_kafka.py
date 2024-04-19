43fe6d8cd62be5d684b442c24e7049f7b74a0f77
swapnilmanjare <swapnilmanjare4@gmail.com>
2024-04-19 18:24:37 +0530

    updated spark file

--- spark_kafka.py
+++ spark_kafka.py
@@ -7,8 +7,8 @@ from pyspark.sql.utils import IllegalArgumentException
 from pyspark.sql.functions import explode, split
 from kafka import KafkaAdminClient, KafkaConsumer, TopicPartition
 
-kafka_brokers = os.environ.get('KAFKA_BROKERS', 'kafka-controller-0.kafka-controller-headless.sparkdemo.svc.cluster.local:9092,kafka-controller-1.kafka-controller-headless.sparkdemo.svc.cluster.local:9092,kafka-controller-2.kafka-controller-headless.sparkdemo.svc.cluster.local:9092')
-kafka_topic = os.environ.get('KAFKA_TOPIC', 'demo-topic')
+kafka_brokers = os.environ.get('KAFKA_BROKERS', 'my-cluster-kafka-0.my-cluster-kafka-brokers.sparkdemo02.svc.cluster.local:9092')
+kafka_topic = os.environ.get('KAFKA_TOPIC', 'my-topic')
 
 
 def create_kafka_topic(topic_name, brokers):
@@ -26,7 +26,7 @@ spark = SparkSession \
     .builder \
     .appName("SparkKafkaDemo") \
     .config("spark.kafka.bootstrap.servers", kafka_brokers) \
-    .config("spark.kafka.sasl.jaas.config", 'org.apache.kafka.common.security.plain.PlainLoginModule required username="controller_user" password="admin123";') \
+    # .config("spark.kafka.sasl.jaas.config", 'org.apache.kafka.common.security.plain.PlainLoginModule required username="controller_user" password="admin123";') \
     .config("spark.kafka.security.protocol", "SASL_SSL") \
     .config("spark.kafka.security.inter.broker.protocol", "SASL_SSL") \
     .getOrCreate()

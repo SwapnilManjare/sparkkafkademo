e4f38880a90878a67bba9d962acd0a3f8d997306
swapnilmanjare <swapnilmanjare4@gmail.com>
2024-04-21 20:54:36 +0530

    asda

---
+++ New-Dockerfile/newapp.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from pyspark import SparkContext, SparkConf
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+from uuid import uuid1
+
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0 pyspark-shell'
+
+
+if __name__ == __main__:
+    sc = SparkContext(appName=”PythonStreamingRecieverKafkaWordCount”)
+    ssc = StreamingContext(sc, 2) # 2 second window
+    broker = my-cluster-kafka-0.my-cluster-kafka-brokers.sparkdemo02.svc.cluster.local:9092
+    topic = my-topic
+    kvs = KafkaUtils.createStream(ssc, \
+                                  broker, \
+                                  “raw-event-streaming-consumer”,\{topic:1}) 
+    lines = kvs.map(lambda x: x[1])
+    counts = lines.flatMap(lambda line: line.split(“ “)) 
+                  .map(lambda word: (word, 1)) \
+                  .reduceByKey(lambda a, b: a+b)
+    counts.pprint()
+    ssc.start()
+    ssc.awaitTermination()
